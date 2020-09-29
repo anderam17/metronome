460d8f2b3c17f291b1660f27828f45080b51897e
Allana Anderson <allana.anderson17@gmail.com>
2020-09-29 09:23:35 -0600

    created slider and bmp display

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,9 +2,19 @@ import React from "react";
 import "./Metronome.css";
 
 function Metronome() {
+
+    let bpm = 100;
+    let playing = false;
+
     return (
         <>
-        <div className="metronome"><h1>HENLO</h1></div>
+        <div className="metronome">
+            <div className="bpm-slider">
+                <div>{bpm} BPM</div>
+                <input type="range" min="60" max="240" value={bpm}/>
+            </div>
+    <button>{playing ? "Stop" : "Start"}</button>
+        </div>
         </>
     )
 }
