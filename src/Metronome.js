a38fdc7f014f323834e82960da83e16b0dbdcc51
Allana Anderson <allana.anderson17@gmail.com>
2020-09-29 09:19:55 -0600

    added initial content to metronome app.

---
+++ src/Metronome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Metronome.css";
+
+function Metronome() {
+    return (
+        <>
+        <div className="metronome"><h1>HENLO</h1></div>
+        </>
+    )
+}
+
+export default Metronome;
