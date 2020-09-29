0bbd3f20dd73aed79c6f37f5a377a21db7fe4375
Allana Anderson <allana.anderson17@gmail.com>
2020-09-29 09:48:24 -0600

    added sounds and tested that they work when played off of my button's onClick event

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import "./Metronome.css";
+import sclick1 from './click1.wav';
+import sclick2 from './click2.wav';
 
 function Metronome() {
   const [metronomeState, setMetronomeState] = useState({
@@ -9,14 +11,25 @@ function Metronome() {
       beatsPerMeasure: 4
   })
 
+  const handleBpmChange = (event) =>{
+    setMetronomeState({...metronomeState, bpm: event.target.value})
+  }
+
+  let click1 = new Audio(sclick1);
+  let click2 = new Audio(sclick2);
+
+  const startStop = () => {
+      click1.play();
+  }
+
   return (
     <>
       <div className="metronome">
         <div className="bpm-slider">
           <div>{metronomeState.bpm} BPM</div>
-          <input type="range" min="60" max="240" value={metronomeState.bpm} />
+          <input type="range" min="60" max="240" value={metronomeState.bpm}  onChange={handleBpmChange}/>
         </div>
-        <button>{metronomeState.playing ? "Stop" : "Start"}</button>
+        <button onClick={startStop}>{metronomeState.playing ? "Stop" : "Start"}</button>
       </div>
     </>
   );
