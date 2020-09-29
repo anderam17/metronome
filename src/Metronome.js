38fdee7b297cde463eff148d9389c74df83734d6
Allana Anderson <allana.anderson17@gmail.com>
2020-09-29 09:34:26 -0600

    added button styling

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,21 +2,20 @@ import React from "react";
 import "./Metronome.css";
 
 function Metronome() {
+  let bpm = 100;
+  let playing = false;
 
-    let bpm = 100;
-    let playing = false;
-
-    return (
-        <>
-        <div className="metronome">
-            <div className="bpm-slider">
-                <div>{bpm} BPM</div>
-                <input type="range" min="60" max="240" value={bpm}/>
-            </div>
-    <button>{playing ? "Stop" : "Start"}</button>
+  return (
+    <>
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm} />
         </div>
-        </>
-    )
+        <button>{playing ? "Stop" : "Start"}</button>
+      </div>
+    </>
+  );
 }
 
 export default Metronome;
